302fc4352d3a0c546a30257bdc76843272f4cd93
fabian bernal <fabian_bernal23222@elpoli.edu.co>
2025-09-17 09:01:32 -0500

    Aplicacion de plantilla

---
+++ ReservasCanchas-Web/wwwroot/js/scripts.js
@@ -0,0 +1,44 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+function setupSidebarToggle() {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle && !sidebarToggle.hasAttribute('data-setup')) {
+        sidebarToggle.setAttribute('data-setup', 'true');
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+}
+
+// Llama a la función al cargar el JS
+setupSidebarToggle();
+
+// Permite que Blazor la llame después de renderizar
+window.setupSidebarToggle = setupSidebarToggle;
