da4a8fb5a2ce689d0da054628aded2266408bf1b
fabian bernal <fabian_bernal23222@elpoli.edu.co>
2025-09-22 08:43:41 -0500

    Se agrega el componente de formulario generico y se hace la prueba con usuariosform

---
+++ ReservasCanchas-Web/Components/Shared/FormularioGenerico.razor.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Reflection;
+
+namespace ReservasCanchas_Web.Components.Shared
+{
+    public partial class FormularioGenerico<TItem> : ComponentBase
+    {
+        [Parameter] public TItem? Model { get; set; }
+        [Parameter] public EventCallback<TItem> OnValidSubmit { get; set; }
+        [Parameter] public EventCallback OnCancel { get; set; }
+        [Parameter] public bool IsEdit { get; set; } = false;
+
+        protected PropertyInfo[] Props => typeof(TItem).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        protected object? GetValue(PropertyInfo prop) => prop.GetValue(Model);
+
+        protected string? GetValueString(PropertyInfo prop)
+        {
+            var value = prop.GetValue(Model);
+            return value?.ToString();
+        }
+        protected int? GetValueInt(PropertyInfo prop)
+        {
+            var value = prop.GetValue(Model);
+            if (value == null) return null;
+            if (value is int i) return i;
+            if (int.TryParse(value.ToString(), out var result)) return result;
+            return null;
+        }
+        protected decimal? GetValueDecimal(PropertyInfo prop)
+        {
+            var value = prop.GetValue(Model);
+            if (value == null) return null;
+            if (value is decimal d) return d;
+            if (decimal.TryParse(value.ToString(), out var result)) return result;
+            return null;
+        }
+        protected DateTime? GetValueDateTime(PropertyInfo prop)
+        {
+            var value = prop.GetValue(Model);
+            if (value == null) return null;
+            if (value is DateTime dt) return dt;
+            if (DateTime.TryParse(value.ToString(), out var result)) return result;
+            return null;
+        }
+        protected bool? GetValueBool(PropertyInfo prop)
+        {
+            var value = prop.GetValue(Model);
+            if (value == null) return null;
+            if (value is bool b) return b;
+            if (bool.TryParse(value.ToString(), out var result)) return result;
+            return null;
+        }
+
+        protected void SetValue(PropertyInfo prop, object? value)
+        {
+            if (Model == null) return;
+            var propType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+            if (value is null)
+            {
+                prop.SetValue(Model, null);
+                return;
+            }
+            if (propType.IsEnum)
+            {
+                prop.SetValue(Model, Enum.Parse(propType, value.ToString()!));
+            }
+            else
+            {
+                prop.SetValue(Model, Convert.ChangeType(value, propType));
+            }
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            if (Model != null)
+                await OnValidSubmit.InvokeAsync(Model);
+        }
+    }
+}
